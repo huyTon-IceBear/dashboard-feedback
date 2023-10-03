ab7fe6a3b0f74af4cc442d276380e1a8c04528f8
huyton <huyton110798@gmail.com>
2023-10-03 11:41:37 +0200

    added template

---
+++ src/components/animate/motion-lazy.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { LazyMotion, m, domMax } from 'framer-motion';
+
+// ----------------------------------------------------------------------
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export function MotionLazy({ children }: Props) {
+  return (
+    <LazyMotion strict features={domMax}>
+      <m.div style={{ height: '100%' }}> {children} </m.div>
+    </LazyMotion>
+  );
+}
