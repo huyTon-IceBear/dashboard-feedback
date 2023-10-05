1376cfdbdff47f88e0a3823857cad63841751d63
huyton <huyton110798@gmail.com>
2023-10-05 13:02:01 +0200

    clean up

--- src/sections/feedback/view/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as FeedbackListView } from './feedback-list-view';
-export { default as FeedbackEditView } from './feedback-edit-view';
-export { default as FeedbackCreateView } from './feedback-create-view';
-export { default as FeedbackDetailsView } from './feedback-details-view';
