fa4eb5d03d7a7dd64549fc91d3412b2e164f93b0
huyton <huyton110798@gmail.com>
2023-10-07 17:18:32 +0200

    clean up

---
+++ src/sections/task/view/index.ts
@@ -0,0 +1,3 @@
+export { default as ProductListView } from './product-list-view';
+export { default as ProductCreateView } from './rfc-template-create-view';
+export { default as ProductDetailsView } from './product-details-view';
