5d7c9db542118c6ec168c2581f7548395fffda0e
huyton <huyton110798@gmail.com>
2023-11-23 23:02:28 +0100

    overide

--- src/api/downloadMedia.ts
+++ src/api/downloadMedia.ts
@@ -1,7 +1,7 @@
 import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
-export async function downloadMediaFile(Key: string, expiresIn = 60) {
+export async function downloadMediaFile(Key: string, expiresIn = 5) {
 	// Ensure that AWS_MONITORING_BUCKET_KEY and AWS_MONITORING_BUCKET_SECRET are strings
 	const accessKeyId = process.env.NEXT_PUBLIC_AWS_MONITORING_BUCKET_KEY as string;
 	const secretAccessKey = process.env.NEXT_PUBLIC_AWS_MONITORING_BUCKET_SECRET as string
